811d5b5c427a424f96446c519687b16a4b178a28
Martin Kirkholt Melhus <martin.melhus@gmail.com>
2014-09-02 16:15:12 +0200

    Add AudioCapture XCode Project

---
+++ AudioCapture/AudioCapture/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  AudioCapture
+//
+//  Created by Martin Kirkholt Melhus on 02/09/14.
+//  Copyright (c) 2014 ___item___. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
